3ddd0346b947b83057cf52f22bbe56326e0e04cd
Josiah ⚡ <devs.josia@gmail.com>
2024-09-25 03:36:43 +0300

    feat: Add greet user functionality!

--- HelloWorld/Program.cs
+++ HelloWorld/Program.cs
@@ -1,2 +1,15 @@
-﻿// See https://aka.ms/new-console-template for more information
+﻿// Welcome the user to app
 Console.WriteLine("Hello, World!");
+Console.WriteLine("This application was built by Josiah");
+Console.WriteLine("=====================================");
+Console.WriteLine();
+
+// Ask the user for their name
+Console.WriteLine("What is your name?");
+string firstName = Console.ReadLine();
+
+// Greet the user
+Console.WriteLine();
+Console.WriteLine($"Hello {firstName}");
+Console.WriteLine("Thank you for using my application");
+Console.ReadLine();
